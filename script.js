58a43c0a1b3941cf2bc1d72a3399d98acb20c2c1
Vikas <vickydelhi03@gmail.com>
2024-08-03 12:07:56 +0530

    css changes

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const humidity = document.getElementById('humidity');
 const wind_speed = document.getElementById('wind-speed');
 const location_not_found = document.querySelector('.location-not-found');
 const weather_body= document.querySelector('.weather-body');
+const place = document.querySelector('.location');
 
 async function checkWeather(city){
     const api_key = '38736de1b1ef79f1c03857a4240addbc';
@@ -14,9 +15,10 @@ async function checkWeather(city){
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
 
-    if(weather_data.cod === '404'){
+    if(weather_data.cod === '404' || city === ''){
         location_not_found.style.display = 'flex';
         weather_body.style.display = 'none';
+        inputbox.value = '';
         return;
     }
 
@@ -32,6 +34,8 @@ async function checkWeather(city){
     humidity.innerHTML = `${weather_data.main.humidity}%`;
 
     wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+
+    place.innerHTML = `<i class="fa-solid fa-location-dot"></i> ${weather_data.name}, ${weather_data.sys.country}`;
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
@@ -53,9 +57,16 @@ async function checkWeather(city){
             weather_img.src = "haze1.png";
             break;
     }
+    inputbox.value = '';
 
 }
 
 searchbtn.addEventListener('click',()=>{
-    checkWeather(inputbox.value)
+    checkWeather(inputbox.value);
+})
+document.addEventListener('keypress',(e)=>{
+    if(e.key === "Enter"){
+        // console.log('Enter Pressed');
+        checkWeather(inputbox.value);
+    }
 })
